d0c807ec1c642afb9d4cab343f9369af40f66d12
KyleRattet <krattet@gmail.com>
2015-09-16 11:15:46 -0600

    updated to make URLS pretty

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+
     $routeProvider
       .when('/add/:number1/:number2', {
         templateUrl: 'partials/addition.html',
@@ -11,6 +12,9 @@ app.config(function($routeProvider) {
         templateUrl: 'partials/division.html',
         controller: 'DivisionController'
       })
+
+       // use the HTML5 History API
+      $locationProvider.html5Mode(true);
 });
 
 
--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,13 @@ app.controller('myController', function($scope) {
   $scope.greeting = "Hellllooooo";
 });
 
-app.controller('AdditionController', function($scope, $routeParams) {
+app.controller('AdditionController', function($scope, $routeParams, $location) {
   $scope.greeting = "This is the addition route.";
   $scope.result = "The sum of the two numbers in the URL is...";
   $scope.number1= $routeParams.number1;
   $scope.number2= $routeParams.number2;
   $scope.parseInt = parseInt;
+  // console.log($route.params);
 });
 
 app.controller('DivisionController', function($scope, $routeParams) {
